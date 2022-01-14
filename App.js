1210850211fb1068553ec2ade6663ff008f7c401
Alex Dresco <alexandra.dresco@ravelin.com>
2022-01-14 10:06:10 +0000

    BE and database ready

--- App.js
+++ App.js
@@ -8,6 +8,12 @@ import CalculatorInput from "./components/CalculatorInput";
 import TravelChecklist from "./components/TravelChecklist";
 
 export default function App() {
+  
+  //Get data from the BE: 
+  // const [data, setData] = useState([])
+  // fetch('/')
+  // .then... and you get use it in the useContext hook if you'd like it on lood :)
+  
   return (
     <View style={styles.container}>
       <h1>T1D Organizer</h1>
