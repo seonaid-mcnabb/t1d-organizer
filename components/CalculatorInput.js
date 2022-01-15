f80d3067a9d38cd3d031cfebdffbfc7cd81f76a9
Seonaid-McNabb <seonaidmcnabb@gmail.com>
2022-01-15 19:40:41 +0100

    Added 3 database tables to database.js

--- components/CalculatorInput.js
+++ components/CalculatorInput.js
@@ -11,59 +11,58 @@ function CalculatorInput() {
     <ScrollView>
       <h1> 5. Calculate </h1>
       <Button title="GO BACK" />
-      <form>
-        <ul>
-          <li>
-            <label for="destination">Destination:</label>
-            <select name="destination" id="destination">
-              <option value="local">local</option>
-              <option value="international">international</option>
-            </select>
-          </li>
 
-          <li>
-            <label for="Duration">Duration:</label>
-            <input name="duration"></input> days
-          </li>
+      <ul>
+        <li>
+          <label for="destination">Destination:</label>
+          <select name="destination" id="destination">
+            <option value="local">local</option>
+            <option value="international">international</option>
+          </select>
+        </li>
 
-          <li>
-            <label for="InsulinUse">Average insulin use:</label>
-            <input name="insulin-use"></input> units/day
-          </li>
+        <li>
+          <label for="Duration">Duration:</label>
+          <input name="duration"></input> days
+        </li>
 
-          <label for="devices">
-            <h2>Devices:</h2>
-          </label>
-          <li>
-            <input
-              type="checkbox"
-              id="MiniMed Insulin Pump"
-              name="MiniMed Insulin Pump"
-              value="MiniMed Insuln Pump"
-            ></input>
-            <label for="MiniMed Insulin Pump">MiniMed Insulin Pump</label>
-          </li>
-          <li>
-            <input
-              type="checkbox"
-              id="Freestyle Libre Sensor"
-              name="Freestyle Libre Sensor"
-              value="Freestyle Libre Sensor"
-            ></input>
-            <label for="Freestyle Libre Sensor">Freestyle Libre Sensor</label>
-          </li>
-          <li>
-            <input
-              type="checkbox"
-              id="Glucometer"
-              name="Glucometer"
-              value="Glucometer"
-            ></input>
-            <label for="Glucometer">Glucometer</label>
-          </li>
-        </ul>
-        <Button title="Generate Checklist" />
-      </form>
+        <li>
+          <label for="InsulinUse">Average insulin use:</label>
+          <input name="insulin-use"></input> units/day
+        </li>
+
+        <label for="devices">
+          <h2>Devices:</h2>
+        </label>
+        <li>
+          <input
+            type="checkbox"
+            id="MiniMed Insulin Pump"
+            name="MiniMed Insulin Pump"
+            value="MiniMed Insuln Pump"
+          ></input>
+          <label for="MiniMed Insulin Pump">MiniMed Insulin Pump</label>
+        </li>
+        <li>
+          <input
+            type="checkbox"
+            id="Freestyle Libre Sensor"
+            name="Freestyle Libre Sensor"
+            value="Freestyle Libre Sensor"
+          ></input>
+          <label for="Freestyle Libre Sensor">Freestyle Libre Sensor</label>
+        </li>
+        <li>
+          <input
+            type="checkbox"
+            id="Glucometer"
+            name="Glucometer"
+            value="Glucometer"
+          ></input>
+          <label for="Glucometer">Glucometer</label>
+        </li>
+      </ul>
+      <Button title="Generate Checklist" />
     </ScrollView>
   );
 }
