68ee8787c1072d5cf852d588d8cb605de7adeed3
Seonaid-McNabb <seonaidmcnabb@gmail.com>
2022-01-16 15:19:29 +0100

    Update home screen

--- App.js
+++ App.js
@@ -1,8 +1,6 @@
 /*
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
-import WelcomeScreen from "./components/WelcomeScreen";
-import ContactList from "./components/ContactList";
 import ContactDetails from "./components/ContactDetails";
 import AddNewContactForm from "./components/AddNewContactForm";
 import CalculatorInput from "./components/CalculatorInput";
@@ -11,14 +9,32 @@ import { NavigationContainer } from "@react-navigation/native";
 */
 
 import * as React from "react";
-import { View, Text } from "react-native";
+import { View, Text, Button } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-function HomeScreen() {
+//Component used in StackNavigator
+import ContactList from "./components/ContactList";
+import CalculatorInput from "./components/CalculatorInput";
+import Calendar from "./components/Calendar";
+
+function WelcomeScreen({ navigation }) {
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Home Screen</Text>
+      <Button
+        title="CONTACTS"
+        onPress={() => navigation.navigate("Contacts")}
+      />
+
+      <Button
+        title="CALENDAR"
+        onPress={() => navigation.navigate("Calendar")}
+      />
+
+      <Button
+        title="CALCULATOR"
+        onPress={() => navigation.navigate("Calculator")}
+      />
     </View>
   );
 }
@@ -29,67 +45,13 @@ function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Home" component={WelcomeScreen} />
+        <Stack.Screen name="Contacts" component={ContactList} />
+        <Stack.Screen name="Calendar" component={Calendar} />
+        <Stack.Screen name="Calculator" component={CalculatorInput} />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
 
 export default App;
-
-//pre-experiment with navigtor
-/*
-export default function App() {
-  //Get data from the BE:
-  // const [data, setData] = useState([])
-  // fetch('/')
-  // .then... and you get use it in the useContext hook if you'd like it on lood :)
-
-  return <NavigationContainer>
-
-  </NavigationContainer>
-  ;
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
-*/
-
-//return area before experiment
-/*
-return (
-  <View style={styles.container}>
-    <h1>T1D Organizer</h1>
-    
-    <WelcomeScreen></WelcomeScreen>
-
-    <ContactList> </ContactList>
-
-    <ContactDetails></ContactDetails>
-
-    <AddNewContactForm></AddNewContactForm>
-
-    <CalculatorInput></CalculatorInput>
-
-    <TravelChecklist></TravelChecklist>
-
-    <StatusBar style="auto" />
-  </View>
-);
-}
-
-const styles = StyleSheet.create({
-container: {
-  flex: 1,
-  backgroundColor: "#fff",
-  alignItems: "center",
-  justifyContent: "center",
-},
-});
-*/
---
+++ components/Calendar.js
@@ -0,0 +1,12 @@
+import react from "React";
+import { View, ScrollView, Button } from "react-native";
+
+function Calendar() {
+  return (
+    <View>
+      <h1>Eventually there will be a calendar here</h1>
+    </View>
+  );
+}
+
+export default Calendar;
