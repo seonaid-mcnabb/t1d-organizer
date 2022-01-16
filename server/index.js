be6c039627b852d818fec9ba4b4054733754dd64
Seonaid-McNabb <seonaidmcnabb@gmail.com>
2022-01-16 12:26:36 +0100

    Added POST route to add new contact and solved routing issue

--- server/index.js
+++ server/index.js
@@ -3,9 +3,48 @@ const app = express();
 const port = 5000;
 const db = require("./model/helper");
 
-app.get("/", (req, res) => {
+//Added in these next lines to try to solve undefined object issue I was having with post method after running npm install --save body-parser
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+//And it works!
+
+//This gets all of the contacts from the database
+app.get("/contacts", (req, res) => {
   db("select * from contacts;")
     .then((results) => res.send(results.data))
+    .catch((err) => res.status(500).send(err));
+});
+
+//This gets all of the appointments from the database
+app.get("/appointments", (req, res) => {
+  db("select * from appointments;")
+    .then((results) => res.send(results.data))
+    .catch((err) => res.status(500).send(err));
+});
+
+//This gets all of the materials/prescriptions from the database
+app.get("/materials", (req, res) => {
+  db("select * from materials;")
+    .then((results) => res.send(results.data))
+    .catch((err) => res.status(500).send(err));
+});
+
+//This adds a new contact to the list
+app.post("/addcontact", (req, res) => {
+  db(
+    `INSERT INTO contacts (firstname, lastname, specialty, phonenumber, email, officename, notes) VALUES ("${req.body.firstname}", "${req.body.lastname}", "${req.body.specialty}","${req.body.phonenumber}","${req.body.email}", "${req.body.officename}","${req.body.notes}");`
+  )
+    .then((result) =>
+      db("SELECT * FROM contacts;") //this is the MySql query
+        .then((results) => {
+          res.send(results.data);
+        })
+    )
     .catch((err) => res.status(500).send(err));
 });
 
